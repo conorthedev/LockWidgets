c0ea2a865bda35a5155c23f85b94ec958b2501f5
Conor <31479541+ConorTheDev@users.noreply.github.com>
2019-10-28 10:09:32 +0000

    Simplify resim usage

--- installtosim.sh
+++ installtosim.sh
@@ -2,4 +2,5 @@ make
 rm -rf /opt/simject/LockWidgets.*
 cp .theos/obj/iphone_simulator/debug/x86_64/LockWidgets.dylib /opt/simject/LockWidgets.dylib
 cp LockWidgets.plist /opt/simject/LockWidgets.plist
-/Users/conor/simject/bin/resim
+# Make sure resim is in /usr/local/bin or on the path somewhere
+resim
